750f68a0eefe9416fea6aa4d6891533023c2b829
aramirez-bodo <aramis@bodo.ai>
2025-02-19 09:34:29 -0600

    AUTOML

---
+++ workbench/ARamirez/prompt_evaluation/azure.sh
@@ -0,0 +1,3 @@
+export AZURE_API_KEY="CLQAXkuwkcBxwpVB0fuL9KmIMGP3GLhTBZcAGezHh6CRZhfqq9fPJQQJ99BBACHYHv6XJ3w3AAAAACOGSJD4"
+export AZURE_BASE_URL="https://ai-arnoldo2137ai929428123129.services.ai.azure.com/models"
+python prompt_evaluation.py "Testing deepseek" "Azure-deepseek" cheatsheet_v2_examples.md tcph_graph.md prompt.txt questions.csv azure DeepSeek-R1
