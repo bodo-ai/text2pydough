4589ca71fc0b79830b42ecd300f5216d94a51ecf
Adriel <adriel@bodo.ai>
2025-03-07 10:02:38 -0600

    Demo GUI Update

--- workbench/AAraya/demo/app.py
+++ workbench/AAraya/demo/app.py
@@ -8,8 +8,8 @@ st.markdown(
         .logo-container {
             position: absolute;
             top: 10px;
-            left: -600px;
-            z-index: 1000; /* Ensures it's always on top */
+            left: -580px;
+            z-index: 1100; /* Ensures it's always on top */
         }
         .logo-container img {
             width: 150px; /* Adjusts the logo size */
