21c0f04420cc36e5e3d4143abf8bf43462f34a79
AraRL <aramirez@tekdatum.com>
2025-03-10 12:21:56 -0500

    AUTOML

--- workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
+++ workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
@@ -206,7 +206,7 @@ def correct(client, question,  code, prompt):
     response= code    
     result, exception= execute_code_and_extract_result(extracted_code, local_env)
     
-    if result is not None:
+    if result is None:
 
         q= (f"""An error occurred while processing this code: {extracted_code}. "
         The error is: '{exception}'. "
