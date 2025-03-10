0f35f36ae498261c05276ae1e6fa235575540061
AraRL <aramirez@tekdatum.com>
2025-03-10 18:12:22 -0500

    AUTOML

--- workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
+++ workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
@@ -208,10 +208,13 @@ def correct(client, question,  code, prompt):
     
     if result is None:
 
-        q= (f"""An error occurred while processing this code: {extracted_code}. "
-        The error is: '{exception}'. "
-        The original question was: '{question}'. "
-        Can you help me fix the issue? Please make sure to use the right syntax and rules for creating pydough code.""")
+        q= (f"""Review and correct the provided Pydough code based on the given exception and original question.
+        Code Block: {extracted_code}. "
+        Exception Message:: '{exception}'. "
+        Original Question:: '{question}'. "
+        - Analyze the provided Pydough code for syntax errors, logical errors, and exceptions.
+        - Identify the specific line or section where the exception occurs. 
+        - Consider the context of the original question to ensure that the code aligns with the intended functionality.""")
 
         response=client.ask(q, prompt)
 
