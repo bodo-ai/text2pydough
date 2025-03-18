0b2f20053e6a692ed12a8839629ef1238d6a2820
AraRL <aramirez@tekdatum.com>
2025-03-18 10:42:58 -0600

    AUTOML

--- workbench/ARamirez/prompt_evaluation/test_data/eval.py
+++ workbench/ARamirez/prompt_evaluation/test_data/eval.py
@@ -7,6 +7,7 @@ import pydough
 from pydough.unqualified import transform_cell
 from pandas.testing import assert_frame_equal, assert_series_equal
 import re
+from concurrent.futures import ThreadPoolExecutor
 
 pydough.active_session.load_metadata_graph(f"{os.path.dirname(__file__)}/tpch_demo_graph.json", "TPCH")
 pydough.active_session.connect_database("sqlite", database=f"{os.path.dirname(__file__)}/tpch.db",  check_same_thread=False)
@@ -233,7 +234,7 @@ def compare_output(folder_path, csv_file_path, db_path):
     def process_and_return(row):
         return process_row(row, db_path)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor() as executor:
         results = list(executor.map(process_and_return, [row for index, row in df.iterrows()]))
 
     # Extract the results into the appropriate columns
