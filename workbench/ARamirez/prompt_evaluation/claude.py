d620db8c2a47675b3a25572c19477c128e124637
AraRL <aramirez@tekdatum.com>
2025-03-20 10:05:32 -0600

    AUTOML

--- workbench/ARamirez/prompt_evaluation/claude.py
+++ workbench/ARamirez/prompt_evaluation/claude.py
@@ -83,7 +83,7 @@ class DeepseekModel:
 
         response = self.brt.converse(modelId= modelId,inferenceConfig= {"maxTokens": 30000,"temperature":self.temperature}, system=system_messages, messages= messages)
         response_text = response["output"]["message"]["content"][0]["text"]
-        reasoning_text= response["output"]["message"]["content"][1]["reasoningContent"]["reasoningText"]["text"]
+        print(reasoning_text= response["output"]["message"]["content"][1].keys())
         print(response.get('usage', {}))
         return response_text, reasoning_text
 
