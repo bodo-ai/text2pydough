af737f92212ee499abf42f3b2c5680d3538fde13
AraRL <aramirez@tekdatum.com>
2025-03-26 11:30:17 -0600

    AUTOML

--- workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
+++ workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
@@ -356,6 +356,8 @@ def main(git_hash):
         #formatted_prompt = prompt.format(script_content=script_content, database_content=database_content, similar_queries=similar_code)
 
         # Process questions
+        questions_df['instructions'] = questions_df['instruction'].fillna('')
+
         questions_df['combined'] = questions_df['question'] + " " + questions_df['instructions']
 
         # Convert the 'combined' column into a list
