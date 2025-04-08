83142e2eb6e48a962698572db2f527dfd9a0ee1c
AraRL <aramirez@tekdatum.com>
2025-04-08 16:34:55 -0600

    AUTOML

--- workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
+++ workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
@@ -176,9 +176,7 @@ def correct(client, question,  code, prompt):
         Can you help me fix the issue? Please make sure to use the right syntax and rules for creating pydough code.""")
 
         response=client.ask(q, prompt)
-        print(response)
         return "".join([code, response])
-    print(response)
     return response
 
     
