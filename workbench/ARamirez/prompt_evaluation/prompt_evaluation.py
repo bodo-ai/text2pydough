731373327e2d6bca312ef8d34725fab6bf3175f1
AraRL <aramirez@tekdatum.com>
2025-04-09 08:30:37 -0600

    AUTOML

--- workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
+++ workbench/ARamirez/prompt_evaluation/prompt_evaluation.py
@@ -235,7 +235,7 @@ def process_question_wrapper(args):
 
 def process_questions(data, provider, model_id, formatted_prompt, questions, temperature, database_content, script_content):
     """ Processes questions in parallel using multiprocessing. """
-    with multiprocessing.Pool(processes=10) as pool:  # Adjust process count as needed
+    with multiprocessing.Pool(processes=1) as pool:  # Adjust process count as needed
         original_responses = pool.map(
             process_question_wrapper, 
             [(provider, model_id, formatted_prompt, data, q, temperature, database_content, script_content) for q in questions]
