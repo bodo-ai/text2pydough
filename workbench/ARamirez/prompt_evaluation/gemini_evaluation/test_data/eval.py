bb24499ac9af1b3822f428fed7d82e3437eda536
AraRL <aramirez@tekdatum.com>
2025-04-23 16:05:42 -0600

    AUTOML

--- workbench/ARamirez/prompt_evaluation/gemini_evaluation/test_data/eval.py
+++ workbench/ARamirez/prompt_evaluation/gemini_evaluation/test_data/eval.py
@@ -152,7 +152,6 @@ def convert_to_df(last_variable):
 def execute_code_and_extract_result(extracted_code, local_env, db_name):
     """Executes the Python code and returns the result or raises an exception."""
     try:
-        print(db_name)
         pydough.active_session.load_metadata_graph(f"{os.path.dirname(__file__)}/{db_name}_graph.json", db_name)
         pydough.active_session.connect_database("sqlite", database=f"{os.path.dirname(__file__)}/{db_name}.db",  check_same_thread=False)
         transformed_source = transform_cell(extracted_code, "pydough.active_session.metadata", set(local_env))
@@ -210,7 +209,6 @@ def process_row(row):
     if pd.notna(extracted_code): 
         local_env = {"pydough": pydough, "datetime": datetime}
         db_name= row["db_name"]
-        print(question, db_name)
 
         result, exception = execute_code_and_extract_result(extracted_code, local_env, db_name)
         
