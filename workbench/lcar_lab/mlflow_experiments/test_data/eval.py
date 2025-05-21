69c95e8aac07bc9b46785e9f445483fc2ea2f0c8
aramirez-bodo <aramis@bodo.ai>
2025-05-21 09:31:54 -0600

    AUTOML

--- workbench/lcar_lab/mlflow_experiments/test_data/eval.py
+++ workbench/lcar_lab/mlflow_experiments/test_data/eval.py
@@ -152,7 +152,7 @@ def execute_code_and_extract_result(extracted_code, local_env, cheatsheet_path, 
         with metadata_lock:
             print(f" In the execute code: {cheatsheet_path}, {db_name}")
             pydough.active_session.load_metadata_graph(cheatsheet_path, db_name)
-            pydough.active_session.connect_database("sqlite", database=database_path)
+            pydough.active_session.connect_database("sqlite", database=database_path, check_same_thread=False)
 
         transformed_source = transform_cell(extracted_code, "pydough.active_session.metadata", set(local_env))
         exec(transformed_source, {}, local_env)
