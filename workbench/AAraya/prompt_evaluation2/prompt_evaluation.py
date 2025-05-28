dcef6acb9c038d775f70ed3909aa9a2c227805cb
Adriel <adriel@bodo.ai>
2025-05-28 16:39:58 -0600

    AUTOML

--- workbench/AAraya/prompt_evaluation2/prompt_evaluation.py
+++ workbench/AAraya/prompt_evaluation2/prompt_evaluation.py
@@ -345,7 +345,7 @@ def main(git_hash):
     kwargs = parse_extra_args(args.extra_args)
     MLFLOW_TRACKING_URI = "http://mlflow-alb-1071096006.us-east-2.elb.amazonaws.com"
     MLFLOW_TRACKING_TOKEN = os.environ["MLFLOW_TRACKING_TOKEN"] 
-    mlflow.gemini.autolog()
+    #mlflow.gemini.autolog()
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     experiment = mlflow.set_experiment(args.experiment_name)
     print("[DEBUG] Iniciando run en MLFlow...")
