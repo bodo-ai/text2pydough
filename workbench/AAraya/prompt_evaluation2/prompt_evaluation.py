4ac4e748c1a306686ce798c5288230f55014d9e4
Adriel <adriel@bodo.ai>
2025-06-02 15:06:36 -0600

    AUTOML

--- workbench/AAraya/prompt_evaluation2/prompt_evaluation.py
+++ workbench/AAraya/prompt_evaluation2/prompt_evaluation.py
@@ -335,7 +335,7 @@ def main(git_hash):
 
     MLFLOW_TRACKING_URI = "http://mlflow-alb-1071096006.us-east-2.elb.amazonaws.com"
     MLFLOW_TRACKING_TOKEN = os.environ["MLFLOW_TRACKING_TOKEN"]
-    mlflow.gemini.autolog()
+    #mlflow.gemini.autolog()
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     experiment = mlflow.set_experiment(args.experiment_name)
 
