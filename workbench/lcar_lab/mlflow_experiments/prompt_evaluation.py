0c09d38078e5c0f247972d08952997fb8e5345f1
aramirez-bodo <aramis@bodo.ai>
2025-06-09 13:20:24 -0600

    AUTOML

--- workbench/lcar_lab/mlflow_experiments/prompt_evaluation.py
+++ workbench/lcar_lab/mlflow_experiments/prompt_evaluation.py
@@ -100,7 +100,7 @@ def format_prompt(prompt, data, question, script, db_name=None, db_markdown_map=
     recommendation = data.get(question, {}).get("context_id", "")
     similar_code = data.get(question, {}).get("similar_queries", "similar pydough code not found")
     question = data.get(question, {}).get("redefined_question", question)
-    return "".join([f"{question}",'\nDatabase schema:\n', str(db_content)]), prompt.format(
+    return "".join([f"{question}",'\nDatabase schema:\n\n', str(db_content)]), prompt.format(
         script_content=script,
         database_content=json_to_markdown(db_content),
         similar_queries=similar_code,
@@ -190,6 +190,8 @@ def main(git_hash):
     parser.add_argument("--model_id", type=str)
     parser.add_argument("--num_threads", type=int)
     parser.add_argument("--extra_args", nargs=argparse.REMAINDER)
+    parser.add_argument("--training_path", type=str)
+    parser.add_argument("--validation_path", type=str)
     args = parser.parse_args()
     kwargs = parse_extra_args(args.extra_args)
 
@@ -402,6 +404,8 @@ def main(git_hash):
             mlflow.log_artifact(combo_path)
             mlflow.log_artifact(complexity_path)
             mlflow.log_artifact(difficulty_path)
+            mlflow.log_artifact(args.validation_path)
+            mlflow.log_artifact(args.training_path)
         mlflow.log_params(filtered_args)
         mlflow.log_params(kwargs)
         mlflow.log_metrics(percentages)
