957d2f7da627a7529e5d13b1f17cc230af842100
gerald-bodoai <geraldnunez@bodo.ai>
2025-06-10 16:13:11 -0600

    Merge second iteration with 3000 samples

--- workbench/lcar_lab/labeling_agent/evaluator_agent_improvement/merge_results.py
+++ workbench/lcar_lab/labeling_agent/evaluator_agent_improvement/merge_results.py
@@ -53,8 +53,8 @@ def main():
     os.makedirs(merged_dir, exist_ok=True)
 
     # Input and output file paths (modify as needed)
-    csv1_path = "/home/gerald8525/repositories/text2pydough/workbench/lcar_lab/labeling_agent/evaluator_agent_improvement/merged_files/20250605_081520/result_match_true.csv"
-    csv2_path = "/home/gerald8525/repositories/text2pydough/workbench/lcar_lab/labeling_agent/evaluator_agent_improvement/merged_files/20250605_080621/result_match_true.csv"
+    csv1_path = "/Users/bodo/Documents/repositories/text2pydough/workbench/lcar_lab/labeling_agent/evaluator_agent_improvement/results/20250608_105939/results.csv"
+    csv2_path = "/Users/bodo/Documents/repositories/text2pydough/workbench/lcar_lab/labeling_agent/evaluator_agent_improvement/results/20250609_135721/results.csv"
   
     output_true = os.path.join(merged_dir, 'result_match_true.csv')
     output_false = os.path.join(merged_dir, 'result_match_false.csv')
