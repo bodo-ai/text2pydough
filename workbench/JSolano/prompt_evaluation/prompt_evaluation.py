eac9cbdbbb6570ca8e92010cdfff16fa75102026
JSolano -Tek <jsolano@tekdatum.com>
2025-06-17 09:18:10 -0600

    AUTOML

--- workbench/JSolano/prompt_evaluation/prompt_evaluation.py
+++ workbench/JSolano/prompt_evaluation/prompt_evaluation.py
@@ -162,7 +162,7 @@ def get_response(client, prompt, data, row, script, db_markdown_map=None, **kwar
     #response= correct(client, formatted_q, response1, formatted_prompt, db_name=db_name)
     return response1, duration, None 
 
-def run_models_parallel(prompt, data, row, script, models_to_test, db_markdown_map=None, tries=3, **kwargs):
+def run_models_parallel(prompt, data, row, script, models_to_test, db_markdown_map=None, tries=1, **kwargs):
     question = row["question"]
     question_idx = row.get("question_index", "?")
     db_name = row.get("db_name", None)
@@ -552,7 +552,7 @@ def main(git_hash):
 
         mlflow.pyfunc.log_model(
             artifact_path=args.model_id,
-            python_model=GeminiWrapper(model_id=args.model_id),
+            python_model="gemini_wrapper.py",
             artifacts={
                 "prompt_file": args.prompt_file,
                 "pydough_file": args.pydough_file,
