0101ffb9f055493d21e76d89572191a6c3a0a6f6
JSolano -Tek <jsolano@tekdatum.com>
2025-06-24 12:36:09 -0600

    Adding add_question_idfile as well as ground truth json adder

---
+++ workbench/JSolano/prompt_evaluation/add_question_id.py
@@ -0,0 +1,17 @@
+import csv
+
+input_file = 'corrected_questions_1.csv'
+output_file = 'corrected_questions_1_with_id.csv'
+
+with open(input_file, 'r', newline='', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+    
+    header = next(reader)
+    # Insert 'question_id' as the first column
+    new_header = ['question_id'] + header
+    writer.writerow(new_header)
+    
+    for idx, row in enumerate(reader, start=1):
+        new_row = [idx] + row
+        writer.writerow(new_row) 
